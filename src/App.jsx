81d3fb914bd520a6dcf198059d34837ee19e16a4
Arthur Mello <arthur.bmello@gmail.com>
2024-05-24 12:57:59 +0000

    change env vars

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { MdOutlineArrowLeft, MdOutlineArrowRight } from "react-icons/md";
 import NewDeal from "./components/NewDeal/NewDeal";
 import Modal from "./components/Modal/Modal";
 import QuestionForm from "./components/QuestionForm/QuestionForm";
+require('dotenv').config()
 
 function App() {
   const [text, setText] = useState("");
