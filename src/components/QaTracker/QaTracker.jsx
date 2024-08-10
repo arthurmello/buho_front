bfbc97ed7ab96a4685ad247d8f6d5955fc52755a
Arthur Mello <arthur.bmello@gmail.com>
2024-08-10 08:06:18 +0200

    additionnal minor fixes

--- src/components/QaTracker/QaTracker.jsx
+++ src/components/QaTracker/QaTracker.jsx
@@ -23,46 +23,46 @@ const QaTracker = ({
     return (
         <>
             <section className={`sidebar ${isShowQuestionsSidebar ? "open" : ""}`}>
-            <div className="sidebar-header" role="button">
-            <p>Q&A Tracker</p>
-            </div>
-            <div className="sidebar-history">
-            {qaTracker.map((history, index) => (
-                <div
-                className="question-box"
-                onClick={() => {
-                    setOwner(history.owner);
-                    setText(history.question);
-                }}
-                key={index}
-                >
-                <p className="question-owner">{index + 1}. Question asked by: {history.owner}</p>
-                <p className="question-text">{history.question}</p>
+                <div className="sidebar-header" role="button">
+                <p>Q&A Tracker</p>
                 </div>
-            ))}
-            </div>
-            <div className="sidebar-info">
-            <div className="sidebar-header" onClick={() => setShowQAModal(true)} role="button">
-                <BiPlus size={20} />
-                <button>Add Question</button>
-            </div>
-            <div className="sidebar-header"
-                onClick={() => resetQaTracker(userIdParam, dealParam, setQaTracker)} role="button">
-                <BiSolidTrash size={20} />
-                <button>Clear Q&A Tracker</button>
-            </div>
-            </div>
+                <div className="sidebar-history">
+                {qaTracker.map((history, index) => (
+                    <div
+                    className="question-box"
+                    onClick={() => {
+                        setOwner(history.owner);
+                        setText(history.question);
+                    }}
+                    key={index}
+                    >
+                    <p className="question-owner">{index + 1}. Question asked by: {history.owner}</p>
+                    <p className="question-text">{history.question}</p>
+                    </div>
+                ))}
+                </div>
+                <div className="sidebar-info">
+                <div className="sidebar-header" onClick={() => setShowQAModal(true)} role="button">
+                    <BiPlus size={20} />
+                    <button>Add Question</button>
+                </div>
+                <div className="sidebar-header"
+                    onClick={() => resetQaTracker(userIdParam, dealParam, setQaTracker)} role="button">
+                    <BiSolidTrash size={20} />
+                    <button>Clear Q&A Tracker</button>
+                </div>
+                </div>
 
-            <GenericModal show={showQAModal} handleClose={() => setShowQAModal(false)}>
-            <QuestionForm
-                onQuestionSubmit={
-                    (data) => onQuestionSubmitted(
-                        data, userIdParam, dealParam, setQaTracker, fetchQaTracker, setShowQAModal
-                    )} 
-                owner={owner}
-                setOwner={setOwner}
-            />
-            </GenericModal>
+                <GenericModal show={showQAModal} handleClose={() => setShowQAModal(false)}>
+                <QuestionForm
+                    onQuestionSubmit={
+                        (data) => onQuestionSubmitted(
+                            data, userIdParam, dealParam, setQaTracker, fetchQaTracker, setShowQAModal
+                        )} 
+                    owner={owner}
+                    setOwner={setOwner}
+                />
+                </GenericModal>
             </section>
         </>
     )
